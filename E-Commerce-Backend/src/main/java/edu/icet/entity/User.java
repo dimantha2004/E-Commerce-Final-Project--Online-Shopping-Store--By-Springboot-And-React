fd7cff2f019aa06115b3c244a70c086ea36a458c
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2025-03-08 00:25:52 +0530

    Dto&Repository&Entity&Enum Done

---
+++ E-Commerce-Backend/src/main/java/edu/icet/entity/User.java
@@ -0,0 +1,49 @@
+package edu.icet.entity;
+
+import edu.icet.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Please Enter Your Name...!")
+    private String name;
+
+    @Column(unique = true)
+    @NotBlank(message = "Please Enter Your Email...!")
+    private String email;
+
+    @NotBlank(message = "Please Enter Your Password...!")
+    private String password;
+
+    @Column(name = "phone_number")
+    @NotBlank(message = "Please Enter Your Phone Number...!")
+    private String phoneNumber;
+
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem>orderItemList;
+
+    @OneToOne(fetch = FetchType.LAZY,cascade =CascadeType.ALL)
+    private RabbitConnectionDetails.Address address;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt=LocalDateTime.now();
+
+}
